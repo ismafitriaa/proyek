9d087b21681433eca50d5faf8bd4bf918d63f2e7
ismafitriaa <risnandari1212@gmail.com>
2022-05-31 13:23:45 +0700

    login dan register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\LogoutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', function () {
+    return view('main');
+});
+
+// // LOGOUT
+// Route::middleware('auth')->group(function(){
+//     Route::resource('tasks', TaskController::class);
+//     Route::post('logout', LogoutController::class);
+// })
